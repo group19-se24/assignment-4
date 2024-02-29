ab8ade0f350b25220265101e387f1dc27dd9a959
Sam <sam.khosravi@hotmail.se>
2024-02-29 15:57:18 +0100

    feat: add test for Accesstoken issue #13

---
+++ api/test/unit/models/test_accesstoken.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.test import TestCase
+
+from oauth2_provider.models import AccessToken
+
+
+class AccesstokenAdminTest(TestCase):
+
+    def test_accesstoken_in_admin(self):
+        #test that ThrottledApplication is in Django admin.
+        self.assertIn(AccessToken, admin.site._registry)
